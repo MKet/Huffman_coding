5007c8cb0b3c156c55d4aa54027889c7e5f61999
Zahharov,Sven S <zahharov@hotmail.com>
2018-03-12 12:37:54 +0100

    Added timer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
 	    System.out.println("Give a string");
 
@@ -14,12 +15,20 @@ public class Main {
         String text = scanner.nextLine();
 
         System.out.println("Processing....");
+        final long startTimeTree = System.currentTimeMillis();
+
         HuffTree tree = new HuffTree(text);
 
+        final long endTimeTree = System.currentTimeMillis();
+        System.out.println("Tree build time: " + (endTimeTree - startTimeTree) );
 
         System.out.println("decompression test");
+        final long startTimeDeco = System.currentTimeMillis();
 
         System.out.println(tree.toString());
+
+        final long endTimeDeco = System.currentTimeMillis();
+        System.out.println("Decompress Time: " + (endTimeDeco - startTimeDeco) );
 
         tree.forEach(System.out::print);
 
